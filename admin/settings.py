f869ddb28da7d7a9bf6ecec7208c01d8c8453767
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-11-27 14:56:28 -0800

    security layer

--- admin/settings.py
+++ admin/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ud6+c7oat14e(kjyfz59yrn=^41jg)$^jo5t5z!*1&3wecaz3f'
-
+SECRET_KEY = str(os.getenv('SECRET_KEY')) 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
